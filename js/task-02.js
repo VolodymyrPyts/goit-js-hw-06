81e42e7e54f2d356901f90ef0dda7babb67a035a
VolodymyrPyts <volodymyr.pyts@gmail.com>
2022-05-16 17:25:58 +0300

    add task#2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,20 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+
+const itemEl = ingredients.map(ingredient => {
+  const item = document.createElement("li")
+  item.classList = "item"
+
+  const paragraphEl =  document.createElement("p")
+  paragraphEl.textContent = `${ingredient}`
+
+  item.append(paragraphEl)
+  
+  return item
+})
+console.log(itemEl)
+const listEl = document.querySelector("#ingredients")
+listEl.append(...itemEl)
+
