ad35683d5e2c7a49810f3b8b40cc522b0cfd32ae
VolodymyrPyts <volodymyr.pyts@gmail.com>
2022-05-17 16:13:34 +0300

    update task #3

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,30 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+const itemEl = images.map(image => {
+  const item = document.createElement("li")
+  item.classList = "gallery__item"
+  item.style.margin = "15px"
+  
+  const imageEl = document.createElement("img")
+  imageEl.setAttribute("src", `${image.url}`)
+  imageEl.setAttribute("alt", `${image.alt}`)
+  imageEl.classList = "gallery__image"
+  imageEl.style.width = "100%";
+  imageEl.style.height = "300px";
+  
+  item.append(imageEl)
+
+  return item
+})
+console.log(itemEl)
+
+const listEl = document.querySelector(".gallery")
+
+listEl.style.display = "flex"
+listEl.style.flexBasis = "calc((100% - 90px) / 3)"
+listEl.style.flexWrap = "wrap";
+listEl.style.margin = "-15px";
+listEl.style.listStyle = "none";
+
+listEl.append(...itemEl)
